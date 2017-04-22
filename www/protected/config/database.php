fc5a3e18945e72d34f737127029f89c149df1029
Xoren <xorenbalasanyan@mail.ru>
2017-04-22 14:58:54 +0400

    fix

--- www/protected/config/database.php
+++ www/protected/config/database.php
@@ -4,14 +4,14 @@
 return array(
         /*
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-         * 
+         *
          */
 	// uncomment the following lines to use a MySQL database
 	'connectionString' => 'mysql:host=localhost;dbname=dbveber',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => '',
+	'password' => 'root',
 	'charset' => 'utf8',
         'tablePrefix' => 'veb_',
-	
+
 );
